eee2696f1e0491c1cecf53a3a882fadf0cac3f1f
Ynardy <ya.lutkova@gmail.com>
2021-07-26 13:21:53 +0600

    first upload

---
+++ Mailchimp/Mailchimp/src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    const reviews= $('#reviews')
+    reviews.owlCarousel({
+        // item:1,
+        // loop:true,
+        // center: true,
+        // dots: false,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+
+    });
+
+});
