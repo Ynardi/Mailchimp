61788c98cfd181f595d0bfda3de975ef4ca52afb
Ynardy <ya.lutkova@gmail.com>
2021-07-29 21:13:30 +0600

    fix: home title

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    const reviews= $('#reviews')
-    reviews.owlCarousel({
-        // item:1,
-        // loop:true,
-        // center: true,
-        // dots: false,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-
-    });
-
-});
-=======
 $('.reviews-carousel').slick({
 
 });
@@ -21,4 +6,3 @@ document.querySelector('.slick-prev').textContent = '';
 document.querySelector('.slick-next').textContent = '';
 
 AOS.init();
->>>>>>> 3c48215748c38f2bb323c117dadecd9e6ec61de9
